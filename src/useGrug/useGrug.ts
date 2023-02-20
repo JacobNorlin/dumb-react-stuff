3e69b6f11de6e2f2cf368d7f11c8bcdec04ab3ef
Jacob Norlin Andersson <jacob.norlinandersson@gmail.com>
2023-02-20 11:26:47 +0100

    Handle cyclic objects and repurposing grug object inside new grug objects
    * Add a symbol to all nested grug objects
    * Prevent re-grugging objects if they are already
      handled by grug
    * Allow reusing portions of grug objects in new
      grug objects. For reused portions the notifier
      should be the same as the component that
      created the original object, meaning that
      component will still re-render if the nested
      object changes

--- src/useGrug/useGrug.ts
+++ src/useGrug/useGrug.ts
@@ -8,10 +8,35 @@ export function useGrug(grugClass: any) {
 }
 
 export function useGrugObject<T extends Record<string, any>>(obj: T) {
+    if ((obj as any)[GrugStuff]) {
+        return obj;
+    }
+
     function addGrugObjWrapper(out: Record<any, any>, o: T) {
         for (let key in o) {
             const val = o[key];
             if (Object.prototype.toString.call(val) === "[object Object]") {
+                //Objects that are already handled by grug can be ignored.
+                //This should prevent cyclic objects from breaking everything
+                if (val[GrugStuff]) {
+                    //In case we are reusing a portion of a grug object
+                    //in a new grug object we still need to assign that
+                    //into the new grug object. Everything below this
+                    //object should already be grugged.
+                    out[key] = val;
+                    continue;
+                }
+                
+                //Append grug stuff to nested objects to allow sending
+                //a portion of a grug object  to a child component.
+                //Unclear exactly how notifiers should work in this case,
+                //but we just reuse the parent notifier since if the parent
+                //component triggers a re-render it should also re-render
+                //the child. Performance may suffer.
+                val[GrugStuff] = {
+                    Notifier: (out as any)[GrugStuff].notifier
+                }
+
                 out[key] = addGrugObjWrapper(out, val);
             } else {
                 Object.defineProperty(out, key, {
